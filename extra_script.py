09c499fc786c20aa20fa2e817d41360f7825bc2e
h2zero <powellperalta@gmail.com>
2022-11-07 20:28:25 -0700

    Add custom UF2 build target.

--- extra_script.py
+++ extra_script.py
@@ -5,3 +5,9 @@ folder = env.GetProjectOption("src_folder")
 env.Replace(
     PROJECT_SRC_DIR="$PROJECT_DIR/src/" + folder
 )
+
+env.AddCustomTarget(
+    "uf2",
+    "$BUILD_DIR/firmware.bin",
+    '"$PYTHONEXE" "$PROJECT_DIR/uf2/uf2conv.py" "$BUILD_DIR/firmware.bin" -c -b 0x00 -f ESP32S3 -o "$PROJECT_DIR/uf2/output/$PIOENV".uf2'
+)
