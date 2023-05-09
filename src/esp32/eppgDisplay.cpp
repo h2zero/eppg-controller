263990bab8b55e467b64aa1a8f82168ebc7e01b2
Zach Whitehead <zjwhitehead@users.noreply.github.com>
2023-05-09 13:52:48 -0500

    display basic time element on screen top

--- src/esp32/eppgDisplay.cpp
+++ src/esp32/eppgDisplay.cpp
@@ -34,9 +34,9 @@ extern EppgThrottle throttle;
 TFT_eSPI tft = TFT_eSPI();
 
 void updateDisplayTask(void * param) {
-  EppgDisplay *tft = (EppgDisplay*)param;
+  EppgDisplay *display = (EppgDisplay*)param;
   for (;;) {
-    tft->update();
+    display->update();
     delay(250); // Update display every 250ms
   }
 
@@ -65,21 +65,23 @@ EppgDisplay::EppgDisplay()
  armAltM(0) {
 }
 
+uint8_t hh = 1, mm = 2, ss = 3; // Get H, M, S from compile time
+uint32_t targetTime = 0;                    // for next 1 second timeout
+byte omm = 99, oss = 99;
+byte xcolon = 0, xsecs = 0;
+
+unsigned int colour = 0;
+
 void EppgDisplay::init() {
   tft.init();
   tft.setRotation(1);
-  tft.fillScreen(TFT_DARKGREY);
-  tft.setTextFont(2);
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextSize(1);
+  tft.setTextColor(TFT_YELLOW, TFT_BLACK);
 
   Serial.println("update disp");
-  tft.fillRectHGradient(0, 0, 160, 50, TFT_MAGENTA, TFT_BLUE);
-  tft.setCursor(10,10);
-  tft.print("Horizontalz gradient");
-
-  tft.fillRectVGradient(0, 60, 160, 50, TFT_ORANGE, TFT_RED);
-  tft.setCursor(10,70);
-  tft.print("Vertical gradient");
-  delay(100);
+  //tft.print("Horizontalz gradient");
+  targetTime = millis() + 1000;
 
   //pinMode(TFT_LITE, OUTPUT);
   //reset();
@@ -259,7 +261,61 @@ uint16_t EppgDisplay::batt2color(int percentage) {
 }
 
 void EppgDisplay::update() {
+ if (targetTime < millis()) {
+    // Set next update for 1 second later
+    targetTime = millis() + 1000;
 
+    // Adjust the time values by adding 1 second
+    ss++;              // Advance second
+    if (ss == 60) {    // Check for roll-over
+      ss = 0;          // Reset seconds to zero
+      omm = mm;        // Save last minute time for display update
+      mm++;            // Advance minute
+      if (mm > 59) {   // Check for roll-over
+        mm = 0;
+        hh++;          // Advance hour
+        if (hh > 23) { // Check for 24hr roll-over (could roll-over on 13)
+          hh = 0;      // 0 for 24 hour clock, set to 1 for 12 hour clock
+        }
+      }
+    }
+
+    // Update digital time
+    int xpos = 120;
+    int ypos = 5; // Top left corner ot clock text, about half way down
+    int ysecs = ypos + 0;
+
+    if (omm != mm) { // Redraw hours and minutes time every minute
+      omm = mm;
+      // Draw hours and minutes
+      if (hh < 10) xpos += tft.drawChar('0', xpos, ypos, 2); // Add hours leading zero for 24 hr clock
+      xpos += tft.drawNumber(hh, xpos, ypos, 2);             // Draw hours
+      xcolon = xpos; // Save colon coord for later to flash on/off later
+      xpos += tft.drawChar(':', xpos, ypos - 2, 2);
+      if (mm < 10) xpos += tft.drawChar('0', xpos, ypos, 2); // Add minutes leading zero
+      xpos += tft.drawNumber(mm, xpos, ypos, 2);             // Draw minutes
+      xsecs = xpos; // Sae seconds 'x' position for later display updates
+    }
+    if (oss != ss) { // Redraw seconds time every second
+      oss = ss;
+      xpos = xsecs;
+
+      if (ss % 2) { // Flash the colons on/off
+        tft.setTextColor(0x39C4, TFT_BLACK);        // Set colour to grey to dim colon
+        tft.drawChar(':', xcolon, ypos - 2, 2);     // Hour:minute colon
+        xpos += tft.drawChar(':', xsecs, ysecs, 2); // Seconds colon
+        tft.setTextColor(TFT_YELLOW, TFT_BLACK);    // Set colour back to yellow
+      }
+      else {
+        tft.drawChar(':', xcolon, ypos - 2, 2);     // Hour:minute colon
+        xpos += tft.drawChar(':', xsecs, ysecs, 2); // Seconds colon
+      }
+
+      //Draw seconds
+      if (ss < 10) xpos += tft.drawChar('0', xpos, ysecs, 2); // Add leading zero
+      tft.drawNumber(ss, xpos, ysecs, 2);                     // Draw seconds
+    }
+  }
 }
 
 // track flight timer
